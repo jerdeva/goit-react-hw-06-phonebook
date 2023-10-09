d4a6193bcba7b65d75cd1b644b5a30883c45b65c
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-10-09 14:10:19 +0300

    PersistGate added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,10 @@
-// import {  useEffect, useState } from "react";
 import { FormPhonebook } from './FormPhonebook/FormPhonebook'
-// import { nanoid } from 'nanoid';
 import { FilterForm } from './FilterForm/FilterForm';
 import { ContactsList } from './ContactsList/ContactsList'
 import React from 'react';
 
 
-// const localStInitialContacts = () => {
-//         const savedContacts = localStorage.getItem('saved-contacts');
-//     if (savedContacts !== null) {
-//       return  JSON.parse(savedContacts)
-//     }
-//     return []
-// }
+
 
 
 export const App = () => {    
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import './index.css';
-import { store } from 'redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistor, store } from 'redux/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+            <PersistGate persistor={persistor} loading={null}>
+        <App />
+        </PersistGate>
     </Provider>
     </React.StrictMode>
 );
